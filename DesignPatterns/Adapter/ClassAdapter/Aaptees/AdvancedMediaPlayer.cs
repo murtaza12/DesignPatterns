a42bac057ea6ddf839937dbe3839eb226c894e4b
murtaza12 <murtaza.mehbobi@gmail.com>
2024-11-21 16:47:15 +0500

    Implemented Class Adapter

---
+++ DesignPatterns/Adapter/ClassAdapter/Aaptees/AdvancedMediaPlayer.cs
@@ -0,0 +1,14 @@
+﻿namespace Adapter.ClassAdapter.Aaptees;
+
+public class AdvancedMediaPlayer
+{
+    public void PlayVlc(string fileName)
+    {
+        Console.WriteLine($"Playing VLC file. Name: {fileName}");
+    }
+
+    public void PlayMp4(string fileName)
+    {
+        Console.WriteLine($"Playing MP4 file. Name: {fileName}");
+    }
+}
---
+++ DesignPatterns/Adapter/ClassAdapter/Adapters/MediaAdapter.cs
@@ -0,0 +1,24 @@
+﻿using Adapter.ClassAdapter.Aaptees;
+using Adapter.ClassAdapter.Targets;
+
+namespace Adapter.ClassAdapter.Adapters;
+
+public class MediaAdapter : AdvancedMediaPlayer, IMediaPlayer
+{
+    public void Play(string audioType, string fileName)
+    {
+        if (audioType.Equals("vlc", StringComparison.OrdinalIgnoreCase))
+        {
+            PlayVlc(fileName); // Using Adaptee's method
+        }
+        else if (audioType.Equals("mp4", StringComparison.OrdinalIgnoreCase))
+        {
+            PlayMp4(fileName); // Using Adaptee's method
+        }
+        else
+        {
+            Console.WriteLine("Invalid media format. Supported formats: VLC, MP4.");
+        }
+    }
+}
+
